3737240e95b0f5f095dcb772e5fd0f20104d5bfb
moham0512 <m.javadfarokhi0512@gmail.com>
2022-05-08 14:21:11 +0430

    todo-list

---
+++ src/Components/TodoInput.jsx
@@ -0,0 +1,57 @@
+import "./Stylesheet/TodoInput.css";
+import { useRef, useState } from "react";
+import React from "react";
+import ReactDOM from "react-dom";
+
+function TodoInput({ tasks, updateTasks }) {
+  const [task, setTask] = useState({ content: "", done: false });
+
+  function inputChangeHandler(event) {
+    setTask({ content: event.target.value, done: false });
+  }
+
+  const todoInput = React.useRef();
+
+  function sendTask() {
+    if (task.content !== "") {
+      tasks.push(task);
+      // const newTasks = new Set(tasks);
+      updateTasks(tasks);
+      setTask({ content: "", done: false });
+      todoInput.current.focus();
+    }
+  }
+
+  function sendTaskEnter(event) {
+    if (event.key === "Enter") {
+      if (task.content !== "") {
+        tasks.push(task);
+        // const newTasks = new Set(tasks);
+        updateTasks(tasks);
+        setTask({ content: "", done: false });
+        todoInput.current.focus();
+      }
+    }
+  }
+  return (
+    <div id="todo-input">
+      <h1>TodoInput</h1>
+      <div id="input">
+        <input
+          ref={todoInput}
+          type="text"
+          placeholder="New Todo"
+          value={task.content}
+          onChange={inputChangeHandler}
+          onKeyPress={sendTaskEnter}
+        />
+        <br />
+        <button type="button" onClick={sendTask}>
+          Add new task
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default TodoInput;
